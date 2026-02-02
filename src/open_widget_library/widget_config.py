c637584f7840314df619190c809f5b3e90fbc462
parker <parker@parkerbritt.com>
2026-02-02 22:32:23 +0000

    feat: add colored icons

--- src/open_widget_library/widget_config.py
+++ src/open_widget_library/widget_config.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from qtpy import QtWidgets, QtCore, QtGui
+from qtpy import QtWidgets, QtCore, QtGui, QtSvg
 border_radius = 10;
 
 class WidgetConfig():
@@ -18,11 +18,34 @@ class WidgetConfig():
     """
         return style_string
 
+    def set_svg_color(self, svg: str, color: str) -> str:
+        return svg.replace(
+            "<svg",
+            f'<svg style="color:{color};"',
+            1
+        )
 
     def get_icon(self, icon_name):
         icon = self._icons.get(icon_name)
+        dir = str(self._ICONS_DIR / f"{icon_name}.svg")
+
+        svg = open(dir, encoding="utf-8").read()
+
+        red_svg = self.set_svg_color(svg, "#e11d48")
+
+        renderer = QtSvg.QSvgRenderer(QtCore.QByteArray(red_svg.encode("utf-8")))
+
         if not icon:
-            icon = QtGui.QIcon(str(self._ICONS_DIR / icon_name))
+            icon = QtGui.QIcon()
+
+            for pixmap_scale in (16,20,24,32,64):
+                pm = QtGui.QPixmap(pixmap_scale, pixmap_scale)
+                pm.fill(QtGui.QColor(0,0,0,0))
+                p = QtGui.QPainter(pm)
+                renderer.render(p)
+                p.end()
+                icon.addPixmap(pm)
+
 
         return icon
 
